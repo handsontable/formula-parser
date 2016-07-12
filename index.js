a1f6ef496753f02d47fc4381901ef21456bf2645
Krzysztof Budnik <budnix@gmail.com>
2016-07-12 10:57:38 +0200

    Export error constants

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 import {Parser, SUPPORTED_FORMULAS} from './src/parser';
-import {default as error} from './src/error';
-import {extractLabel, toLabel, columnIndexToLabel, columnLabelToIndex, rowIndexToLabel, rowLabelToIndex} from './src/helper/cell';
+import {
+  default as error,
+  ERROR,
+  ERROR_DIV_ZERO,
+  ERROR_NAME,
+  ERROR_NEED_UPDATE,
+  ERROR_NOT_AVAILABLE,
+  ERROR_NULL,
+  ERROR_NUM,
+  ERROR_REF,
+  ERROR_VALUE
+} from './src/error';
+import {
+  extractLabel,
+  toLabel,
+  columnIndexToLabel,
+  columnLabelToIndex,
+  rowIndexToLabel,
+  rowLabelToIndex
+} from './src/helper/cell';
 
 export {
   SUPPORTED_FORMULAS,
+  ERROR,
+  ERROR_DIV_ZERO,
+  ERROR_NAME,
+  ERROR_NEED_UPDATE,
+  ERROR_NOT_AVAILABLE,
+  ERROR_NULL,
+  ERROR_NUM,
+  ERROR_REF,
+  ERROR_VALUE,
   Parser,
   error,
   extractLabel,
