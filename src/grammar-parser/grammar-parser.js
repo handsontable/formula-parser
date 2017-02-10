560e5f8e7e8b6ca3c056e2f8cf1e80a06659d857
Krzysztof Budnik <budnix@gmail.com>
2017-02-10 10:49:01 +0100

    Removed tagged expressions from gramma parser

--- src/grammar-parser/grammar-parser.js
+++ src/grammar-parser/grammar-parser.js
@@ -331,7 +331,6 @@ parse: function parse(input) {
         lstack.length = lstack.length - n;
     }
 
-_token_stack:
     var lex = function () {
         var token;
         token = lexer.lex() || EOF;
@@ -358,7 +357,6 @@ _token_stack:
             action = table[state] && table[state][symbol];
         }
 
-_handle_error:
         // handle parse error
         if (typeof action === 'undefined' || !action.length || !action[0]) {
             var error_rule_depth;
